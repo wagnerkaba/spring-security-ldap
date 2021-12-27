e7942261486113921db88a3ebd733f041fecc16c
wagnerkaba <bom.link+github@gmail.com>
2021-12-27 17:43:35 -0300

    implementação das classes principais

--- src/main/java/com/wagner/springsecurityldap/SpringSecurityLdapApplication.java
+++ src/main/java/com/wagner/springsecurityldap/SpringSecurityLdapApplication.java
@@ -3,11 +3,15 @@ package com.wagner.springsecurityldap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringSecurityLdapApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityLdapApplication.class, args);
 	}
+
+	//TODO PROGRAMA NÃO ESTÁ FUNCIONANDO. CORRIGIR BUG
+
 
 }
